96efbe4822eee8622b4816a95760d8bf02a97db5
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-05-21 12:27:41 +0300

    Task_LINQ

--- Task_LINQ/Task_LINQ/Program.cs
+++ Task_LINQ/Task_LINQ/Program.cs
@@ -10,16 +10,71 @@ namespace Task_LINQ
     {
         static void Main(string[] args)
         {
-            ListHelper<IAnimalWorld> listOfAnimals = new ListHelper<IAnimalWorld>();
-            listOfAnimals.AddItem(new Dog());
-            listOfAnimals.AddItem(new Whale());
-            listOfAnimals.AddItem(new Crow());
-            listOfAnimals.AddItem(new Eagle());
-            listOfAnimals.AddItem(new Bettle());
-            listOfAnimals.AddItem(new Spider());
+            List<IAnimalWorld> listOfAnimals = new List<IAnimalWorld>
+            {
+                new Dog(),
+                new Whale(),
+                new Crow(),
+                new Eagle(),
+                new Bettle(),
+                new Spider()
+            };
 
             Console.WriteLine("/Selecting animals with 6-letter names:/");
-            //IEnumerable<ListHelper<IAnimalWorld>> sequence = 
+            var selectedAnimalsByLetters = from animal in listOfAnimals
+                                  where animal.Name.Length == 6
+                                  select animal;
+            foreach (IAnimalWorld animal in selectedAnimalsByLetters)
+                Console.WriteLine("{0}", animal.Name);
+            Console.WriteLine("///////////////////////////////////////////////////////////");
+
+            Console.WriteLine("/Selecting animals that don't have more than 3 limbs:/");
+            var selectedAnimalsByLimbs = from animal in listOfAnimals
+                                           where animal.NumberOfLimbs <= 3
+                                           select animal;
+            foreach (IAnimalWorld animal in selectedAnimalsByLetters)
+                Console.WriteLine("{0} - {1} limbs", animal.Name, animal.NumberOfLimbs);
+            Console.WriteLine("///////////////////////////////////////////////////////////");
+
+            Console.WriteLine("/Sorting list of animals by number of limbs (descending):/");
+            var sortedAnimalsByNumberOfLimbs = from animal in listOfAnimals
+                                                 orderby animal.NumberOfLimbs descending
+                                                 select animal;
+            foreach (IAnimalWorld animal in sortedAnimalsByNumberOfLimbs)
+                Console.WriteLine("{0} - {1} limbs", animal.Name, animal.NumberOfLimbs);
+            Console.WriteLine("///////////////////////////////////////////////////////////");
+
+            Console.WriteLine("/Sorting list of animals by names (ascending):/");
+            var sortedAnimalsByName = from animal in listOfAnimals
+                                                 orderby animal.Name
+                                                 select animal;
+            foreach (IAnimalWorld animal in sortedAnimalsByName)
+                Console.WriteLine("{0}", animal.Name);
+            Console.WriteLine("///////////////////////////////////////////////////////////");
+
+            Console.WriteLine("/Sorting list of animals by length of names (ascending):/");
+            var sortedAnimalsByNameLength = from animal in listOfAnimals
+                                      orderby animal.Name.Length
+                                      select animal;
+            foreach (IAnimalWorld animal in sortedAnimalsByNameLength)
+                Console.WriteLine("{0}", animal.Name);
+            Console.WriteLine("///////////////////////////////////////////////////////////");
+
+            Console.WriteLine("/Number of elements in list:/");
+            var sizeOfList = (from animal in listOfAnimals
+                              select animal).Count();
+            Console.WriteLine("{0}", sizeOfList);
+            Console.WriteLine("///////////////////////////////////////////////////////////");
+
+            Console.WriteLine("/Minimum number of limbs:/");
+            var min = listOfAnimals.Min(m => m.NumberOfLimbs);
+            Console.WriteLine("{0}", min);
+            Console.WriteLine("///////////////////////////////////////////////////////////");
+
+            Console.WriteLine("/Maximum number of limbs:/");
+            var max = listOfAnimals.Max(m => m.NumberOfLimbs);
+            Console.WriteLine("{0}", max);
+            Console.WriteLine("///////////////////////////////////////////////////////////");
 
             Console.ReadKey();
         }
